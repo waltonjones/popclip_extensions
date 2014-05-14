c1f5e027ece4d4dae3f01672dfc66c8f9c46ff49
Walton Jones <waltonjones@kaist.edu>
2014-05-14 13:02:18 +0900

    Improved speed and removed new lines after paper id

--- PMID-DOI.popclipext/pmiddoi.py
+++ PMID-DOI.popclipext/pmiddoi.py
@@ -1,24 +1,22 @@
 import os
 import urllib2
 import json
+import sys
 
-# input = os.environ['POPCLIP_TEXT']
-input = "10.1371/journal.pgen.1001346"
-
+input = os.environ['POPCLIP_TEXT']
 
 request = urllib2.Request("http://pmid2doi.labs.crossref.org/" + input, headers={"Accept": "application/json"})
 try:
     response = urllib2.urlopen(request)
 except urllib2.HTTPError, e:
-    print 'That ID is not the database. :('
-    print 'Error code: ', e.code
+    print 'Could not locate paper ID.'
+    print 'Error code: ', e.code,
 except urllib2.URLError, e:
     print 'We failed to reach the server.'
-    print 'Reason: ', e.reason
+    print 'Reason: ', e.reason,
 else:
-    response = response.read()
-    response_dict = json.loads(response)
+    response = json.loads(response.read())
     if "/" in input:
-        print(response_dict['mapping']['pmid'])
+        sys.stdout.write(str(response['mapping']['pmid']))
     else:
-        print(response_dict['mapping']['doi'])
+        sys.stdout.write(str(response['mapping']['doi']))
